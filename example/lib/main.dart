a5f359263c9e250059f02563ed80860c8aedefd5
Alex Li <github@alexv525.com>
2025-08-21 23:47:57 +0800

    Add Dart example

---
+++ example/lib/main.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: unused_elements, unused_local_variable
+
+void main() {
+  var name = 'John';
+  final age = 25;
+  final age1 = 25.0;
+  final list = [1, 2, 3];
+
+  // No hints shown (explicit type)
+  String explicitName = "Jane";
+  int explicitAge = 18;
+
+  final foo = Foo('bar1', bar2: 'bar2', bar3: 'bar3');
+  final foobar1 = foo.bar1;
+  final foobar2 = foo.bar2;
+  final foobar3 = foo.bar3;
+  final (fb1, fb2, fb3) = foo.toRecord();
+}
+
+class Foo {
+  const Foo(
+    this.bar1, {
+    required this.bar2,
+    this.bar3,
+  });
+
+  final String bar1;
+  final String bar2;
+  final String? bar3;
+
+  (String, String, String?) toRecord() => (bar1, bar2, bar3);
+}
